1af071213120fe9e839843b09685daf1bb82092b
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2025-04-06 18:35:28 +0300

    add a logger to see the message of the started app

--- src/main/java/com/ibrahim/platform/PlatformApplication.java
+++ src/main/java/com/ibrahim/platform/PlatformApplication.java
@@ -1,13 +1,16 @@
 package com.ibrahim.platform;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformApplication.class, args);
+		log.info("Platform Application Started");
 	}
 
 }
