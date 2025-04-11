d2eb9d4955c9979da8f5391215e8c46ec71f9ede
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2025-04-11 21:46:49 +0300

    Create the StorageException class

---
+++ src/main/java/com/ibrahim/platform/exceptions/StorageException.java
@@ -0,0 +1,19 @@
+package com.ibrahim.platform.exceptions;
+
+public class StorageException extends Exception{
+
+    public StorageException() {
+    }
+
+    public StorageException(Throwable cause) {
+        super(cause);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+}
