c20e503ca42e6cddeed9c30e4a70df5c941576b5
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2025-04-12 16:21:19 +0300

    Create PhotoDto class

---
+++ src/main/java/com/ibrahim/platform/domain/dtos/PhotoDto.java
@@ -0,0 +1,20 @@
+package com.ibrahim.platform.domain.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class PhotoDto {
+
+    private String url;
+    private LocalDateTime uploadDate;
+}
