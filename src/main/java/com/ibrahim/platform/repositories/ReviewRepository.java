25b3de1ed49a1bb8f4d1b4d633ec60e39c9a31dc
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2025-04-12 17:31:04 +0300

    delete the unused files (we will created again when we need them)

--- src/main/java/com/ibrahim/platform/repositories/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibrahim.platform.repositories;
-
-import com.ibrahim.platform.domain.entities.Review;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends ElasticsearchRepository<Review, String> {
-}
